6da98356e71a91be4b57e61be49bc76fb72a6d81
dragove <dragove@qq.com>
2024-12-14 17:42:09 +0800

    build: update dependencies

--- bifer/src/bifer/Hello.scala
+++ bifer/src/bifer/Hello.scala
@@ -1,22 +1,16 @@
 package bifer
 
-import sqala.dsl.*
+import sqala.static.dsl.*
 import sqala.jdbc.*
 import bifer.DB.db
-import sqala.dsl.statement.query.SelectQuery
 import sqala.printer.PostgresqlDialect
 import java.util.Arrays
-import sqala.dsl.annotation.autoInc
-import sqala.dsl.annotation.primaryKey
-
-extension (exprS: Expr[String])
-  def len = length(exprS)
-  def toUp = upper(exprS)
+import sqala.static.annotation.*
 
 case class User(
     @primaryKey
     @autoInc
-    id: Option[Long],
+    id: Long,
     name: String,
     email: String,
     category: String,
@@ -26,23 +20,17 @@ case class User(
 )
 
 def fetchUser(email: String) =
-  val q = query[User]
-    .filter(_.email == email)
-    .leftJoin[User]((a, b) => a.name == b.name)
-    .groupBy((a, b) => (name = a.name))
-    .map((g, a, b) => (name = g.name))
+  val q = queryContext:
+    query[User]
+      .filter(_.email == email)
   val sql = q.sql(PostgresqlDialect)
   println(sql(0))
   println(Arrays.toString(sql(1)))
   db.fetch(q).head
 
 @main def main: Unit =
-  db.execute(
-    insert(
-      List(
-        User(None, "a", "b", "c", "d", 1.0, false),
-        User(None, "a", "b", "c", "d", 1.0, false)
-      )
-    )
-  )
-  fetchUser("dove@qq.com")
+  val x = (name = "hi", age = 10)
+  println(x.age)
+  val u = fetchUser("dove@qq.com")
+  println(u.name)
+
--- build.sc
+++ build.sc
@@ -1,26 +1,25 @@
 import mill._, scalalib._
 
 trait CommonModule extends ScalaModule {
-  def scalaVersion = "3.6.1"
+  def scalaVersion = "3.6.2"
   def scalacOptions = Seq(
     "-Werror",
     "-feature",
     "-Wsafe-init",
     "-Wunused:all",
     "-deprecation",
-    "-experimental",
     "-Yexplicit-nulls",
     "-language:strictEquality",
+    "-language:experimental.namedTuples",
   )
 }
 
 object bifer extends CommonModule {
-  val sqalaVersion = "0.1.4"
+  val sqalaVersion = "0.2.3"
   def ivyDeps = Agg(
-    ivy"com.wz7982::sqala-dsl:$sqalaVersion",
     ivy"com.wz7982::sqala-jdbc:$sqalaVersion",
     ivy"com.wz7982::sqala-dynamic:$sqalaVersion",
-    ivy"com.softwaremill.sttp.tapir::tapir-core:1.11.2",
+    ivy"com.softwaremill.sttp.tapir::tapir-core:1.11.10",
     ivy"com.zaxxer:HikariCP:5.1.0",
     ivy"org.postgresql:postgresql:42.7.4",
     ivy"org.apache.logging.log4j:log4j-slf4j2-impl:2.23.1"
@@ -28,7 +27,7 @@ object bifer extends CommonModule {
 
   object test extends ScalaTests with TestModule.Munit {
     def ivyDeps = Agg(
-      ivy"org.scalameta::munit:1.0.1"
+      ivy"org.scalameta::munit:1.0.3"
     )
   }
 }
